2cc499c044e2587d74829d1e848eff1fc817ac8a
Dan Erickson <derickson82@gmail.com>
2014-03-04 16:21:46 -0700

    New gradle build setup

---
+++ android/gen/com/badlogicgames/gradletest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.badlogicgames.gradletest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ desktop/src/thwack/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package thwack.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import thwack.ThwackGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ThwackGame(), config);
+	}
+}
