df51a14dce2db5b14a509258c873d1b49ae84762
oonej <chadz777@gmail.com>
2014-12-13 17:55:35 -0500

    Fixed gradle and started new base line

---
+++ desktop/src/thwack/desktop/Packer.java
@@ -0,0 +1,15 @@
+package thwack.desktop;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
+
+public class Packer {
+	public static void main(String[] args) {
+		Settings settings = new Settings();
+		settings.pot = true;
+		settings.stripWhitespaceX = true;
+		settings.stripWhitespaceY = true;
+		settings.combineSubdirectories = true;
+		TexturePacker.process(settings, "C:/Users/super radish/git/thwack/desktop/assets/Saw Rat", "C:/Users/super radish/git/thwack/desktop/assets/Saw Rat-Packed", "Saw Rat");
+	}
+}
